f93efcd6f28223fb7a3a85ec25acfa100658a5f8
chaima <chamabayroune@gmail.com>
2021-11-12 12:27:46 +0100

    challenge

---
+++ challenge 7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;b;
+	printf ("etrer la valeur de a:\n b: \n");
+	scanf ("%d %d",&a,&b);
+	printf ("a+b=%d",a+b);
+	printf ("a-b=%d",a-b);
+	printf ("a*b=%d",a*b);
+	printf ("a/b=%d",a/b);
+	printf ("a%b=%d",a%b);
+	return 0;
+}
