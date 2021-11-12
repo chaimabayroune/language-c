44020725305f49ba9d3eb08699c48839c2870e36
chaimabayroune <93934009+chaimabayroune@users.noreply.github.com>
2021-11-12 12:33:58 +0100

    Update challenge 7.c

--- challenge 7.c
+++ challenge 7.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;b;
+	int a,b;
 	printf ("etrer la valeur de a:\n b: \n");
 	scanf ("%d %d",&a,&b);
 	printf ("a+b=%d",a+b);
